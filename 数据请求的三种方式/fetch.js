ccbc1c3ef3ad7be9019f2f199264a2ee6a3068c3
stephen <stephen@163.com>
2018-11-22 23:19:55 +0800

    数据请求完善

--- 数据请求的三种方式/fetch.js
+++ 数据请求的三种方式/fetch.js
@@ -19,3 +19,32 @@ fetch(url).then(reponse => {
 }).catch(err => {
     console.log(err);
 })
+
+// 缺点:
+// 1.http错误处理  当返回的是一个错误的http状态码是，是不会被reject的，即使该响应返回的状态码是404或500，相反它会标记promise的状态设置为resolve,
+// 但resolve返回的ok值会被标记为false,仅当网络错误或者请求被拒绝时才会被标记为reject
+// 2.默认情况下不会从服务端接受或发送任何cookie，要发送cookie必须设置credentials选项
+// 3.fetch不支持超时控制
+
+
+// fetch集合async和await使用
+async function getData ( url){
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+}
+
+// async/await是ES7的API，目前还在试验阶段，还需要我们使用babel进行转译成ES5代码
+
+// fetch封装
+// jquery ajax
+$.post(url, {name: 'test'})
+// fetch
+fetch(url, {
+    method: 'POST',
+    body: Object.keys({name: 'test'}).map((key) => {
+        return encodeURIComponent(key) + '=' + encodeURIComponent(params[key]);
+    }).join('&')
+})
+// 此外fetach不支持超时控制
+
--- 数据请求的三种方式/jqueryAjax.js
+++ 数据请求的三种方式/jqueryAjax.js
@@ -8,3 +8,6 @@ $.ajax({
 })
 
 // 是对原生XHR的封装，支持JSONP，非常方便，
+// jsonp为什么不支持跨域
+// JSONP的最基本的原理是：动态添加一个<script>标签，而script标签的src属性是没有跨域的限制的。这样说来，这种跨域方式其实与ajax XmlHttpRequest协议无关了。
+// 可以说jsonp的方式原理上和<script src="http://跨域/...xx.js"></script>是一致的，因为他的原理实际上就是 使用js的script标签 进行传参，那么必然是get方式的了，和浏览器中敲入一个url一样
