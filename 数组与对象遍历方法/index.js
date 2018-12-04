088caeabcdae34780461f721c528da70a556805e
dcy <18964580031@163.com>
2018-12-04 19:22:10 +0800

    数组遍历完善

--- 数组与对象遍历方法/index.js
+++ 数组与对象遍历方法/index.js
@@ -45,12 +45,97 @@ for(var i=0;i<arrTmp.length;i++){
     }
 }
  
-//会输出value1 value2 value3
-arrTmp.forEach(function(value){
+//会输出value1 value2 value3,forEach无返回值,返回undefined,且会改变原数组
+var tempArr =  arrTmp.forEach(function(value){
     console.log(value);
     if(value==1){
         return;
     }
+});
+
+console.log(tempArr)//undefined
+
+var arr = [12,23,24,42,1];
+// var res = arr.forEach(function (item,index,input) {
+//     input[index] = item*10;
+// })
+// console.log(res)//undefined
+// console.log(arr);//[ 120, 230, 240, 420, 10 ]
+
+
+// map有返回值，可以return出来,且不会改变原数组
+var res2 = arr.map(function (item,index,inout) {
+    return item*10;
 })
+console.log(res2)//[ 120, 230, 240, 420, 10 ]
+console.log(arr);//[ 12, 23, 24, 42, 1 ]
+
+
+// 不管是forEach还是map在IE6-8都不兼容（不兼容的情况在Array.prototype上没有这两个方法），那么需要我们自己封装一个兼容的方法，代码如下：
+
+// 封装forEach
+Array.prototype.myForEach = function myForEach(callback,context) {
+    var context = context || window;
+    if('forEach' in Array.prototype){
+        this.forEach(callback,context);
+        return;
+    }
+
+    // IE6-8
+    for (let index = 0; index < this.length; index++) {
+       callback && callback.call(context,this[index],index,this) 
+    }
+}
+
+
+// 封装map
+Array.prototype.myMap = function myMap(callback,context) {
+    var context = context || window;
+    if('map' in Array.prototype){
+        this.map(callback,context);
+        return;
+    }
+
+    // IE6-8
+    var newArr = [];
+    for (let index = 0,len = this.length; index < this.length; index++) {
+       if(typeof callback === 'function'){
+           var val = callback.call(context,ths[index],index,this);
+           newArr[newArr.length] = val;
+       }
+    }
+    return newArr;
+}
+
+
+// 类数组转换成数组的方法：4种
+// [].prototype.slice.call(likeArr,0);
+// [...likeArr]
+// let arr2 = Array.from(likeArr);
+// for (let index = 0; index < likeArr.length; index++) {
+//    arr2.push(likeArr[index])
+// }
+
+
+// slice和splice的区别
+// slice返回子数组，不会影响原数组,可以看成截取数组，同样可以同于字符串的且切割
+// start参数：必须，规定从何处开始选取，如果为负数，规定从数组尾部算起的位置，-1是指最后一个元素。
+// end参数：可选（如果该参数没有指定，那么切分的数组包含从start倒数组结束的所有元素，如果这个参数为负数，那么规定是从数组尾部开始算起的元素）。
+var testArr = [1,2,3,4,5];
+console.log(testArr.slice(1))//[2,3,4,5]
+console.log(testArr.slice(1,3))//[2,3]
+console.log(testArr)//[1,2,3,4,5]
+console.log(('hello world').slice(1,5))//ello
+
+
+// splice可以看作删除数组，会改变原数组
+// index参数：必须，整数，规定添加或者删除的位置，使用负数，从数组尾部规定位置。
+// howmany参数：必须，要删除的数量，如果为0，则不删除项目。
+// tem1,...itemX参数：可选，向数组添加的新项目。
+var testArr2 = [1,2,3,4,5];
+console.log(testArr2.splice(2,1,'hello'));//[3]
+console.log(testArr2);//[ 1, 2, 'hello', 4, 5 ]
+
+
 
 
