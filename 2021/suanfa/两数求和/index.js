288a4f769302230cffc2f2ec6142e4b321a5a34d
dingcy <dingcy>
2021-03-02 23:27:17 +0800

    双指针（有序+数组）

---
+++ 2021/suanfa/两数求和/index.js
@@ -0,0 +1,14 @@
+const twoSum = function(nums, target) {
+    let map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        let temp = target - nums[i];
+       if(map.get(temp) !== undefined){
+            return [map.get(temp),i]
+       }
+       map.set(nums[i],i)
+        
+    }
+}
+
+
+console.log(twoSum([2, 7, 11, 15],9))
